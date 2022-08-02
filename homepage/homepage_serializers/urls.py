6ea0825d1cfdefb909abda760153efd6f47cd8bd
mayank <mayank.infograins@gmail.com>
2022-08-02 18:02:40 +0530

    remove company from url

--- homepage/homepage_serializers/urls.py
+++ homepage/homepage_serializers/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("hero-section-content/",HeroSectionAPI.as_view(),name="hero-section-content"),
     path("notable-blockchain-platform/",NotableBlockchainPlatformsAPI.as_view(),name="notable-blockchain-platform"),
     path('why-we-choose/',WhyChooseUsAPI.as_view(),name='why-we-choose'),
-    path('blockchain-development-process/',BlockchainDevelopmentProcessAPI.as_view())
+    path('blockchain-development-process/',BlockchainDevelopmentProcessAPI.as_view()),
     path('what-we-do/',WhatWeDoAPI.as_view(),name='what-we-do')
 
 ]
